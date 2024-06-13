93a2ee9c3de5715ca6b3e02383a597ec2da08c99
KarpenkoMax <karpenkomaxd@yandex.ru>
2024-06-13 06:19:39 +0300

    пояснение + форматирование вывода

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from datetime import datetime
 """
 ПОЯСНЕНИЯ
 
+данные в базе в процессе отладки изменились, но при желании можно заменить файл в папке db
+
 assumptions
 1. Все операции по фильтрации, агрегации и т.п., кроме подстановки даты, должны быть сделано при помощи SQL
-2. Рассылки по регистрации и по активности - 2 разные сущности, так что одному пользователю может быть отправлено что-то одно, или оба сообщения сразу
+2. Рассылки по регистрации и по активности - 2 разные сущности, так что одному пользователю может быть отправлено что-то одно, или оба сообщения сразу.
+Это, в свою очередь, усложняет учет отправленных сообщений
 3. В один и тот же день письма по активности одному и тому же пользователю не отправлялись - в противном случае записи в результате будут дублироваться,
-   что проще отлавливать уже программно
+   что, при уже достаточной сложности скрипта, проще отлавливать уже в программе
+4. По алгоритму для в.2 - сообщение отправится, если прошло нужное кол-во дней и с последней активности по введенную дату:
+    - либо не было сообщений по активности
+    - либо были сообщения о меньшем кол-ве дней
 
 """
 
@@ -60,6 +66,8 @@ if __name__ == '__main__':
     ondate = datetime(2024, 6, 15)
     print(f'For date {ondate.strftime('%Y-%m-%d')}:')
 
+    template = "{0:8}|{1:20}|{2:35}|{3:35}"
+
     # вариант 1 (если отправлялись сообщения - больше не шлём)
     res_list = fetch_user_message_list(ondate, on_existing_sent='skip')
 
@@ -67,9 +75,9 @@ if __name__ == '__main__':
     print_list = [(x[0], x[1], x[8], x[9]) for x in res_list]
 
     print('Список по в.1')
-
+    print(template.format('user_id', 'username', 'reg_message', 'activity_message'))
     for rec in print_list:
-        print(rec)
+        print(template.format(rec[0], rec[1], rec[2] if rec[2] is not None else '-', rec[3] if rec[3] is not None else '-'))
 
     # вариант 2 (если отправлялись сообщения - отправлять, если прошел срок)
     res_list = fetch_user_message_list(ondate, on_existing_sent='recalc')
@@ -77,6 +85,6 @@ if __name__ == '__main__':
     print_list = [(x[0], x[1], x[8], x[9]) for x in res_list]
 
     print('Список по в.2')
-
+    print(template.format('user_id', 'username', 'reg_message', 'activity_message'))
     for rec in print_list:
-        print(rec)
+        print(template.format(rec[0], rec[1], rec[2] if rec[2] is not None else '-', rec[3] if rec[3] is not None else '-'))
